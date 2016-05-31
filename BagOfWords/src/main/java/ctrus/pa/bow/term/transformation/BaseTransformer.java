0e8897b3d0fbde1b5fb669af76122f7fd6ea75ad
Naveen Kulkarni <naveen.kulkarni@research.iiit.ac.in>
2016-05-31 16:06:44 +0530

    small change in the copyright header in all source files

--- BagOfWords/src/main/java/ctrus/pa/bow/term/transformation/BaseTransformer.java
+++ BagOfWords/src/main/java/ctrus/pa/bow/term/transformation/BaseTransformer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016 
+ * Copyright (c) 2015, 2016  Naveen Kulkarni
  *
  * This file is part of Bag of Words program. 
  *
